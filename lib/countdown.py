aec88caf75e63cb0976912f0d2fc89f1f65a408a
ussenuk <ukimanuka@gmail.com>
2024-01-05 12:10:32 +0300

    understanding while loop

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import time
+
+def countdown(second):
+    # second= 20
+    while second >= 1:
+        print(f'{second} SECOND(S)!')
+        second -= 1
+    return print("HAPPY NEW YEAR!") 
+
+
+def countdown_with_sleep(number):
+    # number= 20
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        time.sleep(1)
+        number -= 1
+    return print("HAPPY NEW YEAR!") 
+
